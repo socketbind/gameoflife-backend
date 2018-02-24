d3d5c997e4464b9de618bd23733007fcf22843bf
Gabriel Miklós <socketbind@gmail.com>
2018-02-25 00:21:19 +0100

    - Add eslint and jest. - Introduce gameoflife module with habitat implementation.

---
+++ gameoflife/__tests__/habitat.test.js
@@ -0,0 +1,139 @@
+const Habitat = require('../habitat');
+
+test('can create empty habitat', () => {
+  const habitat = Habitat.empty(3, 3);
+  expect(habitat).toBeDefined();
+});
+
+test('creates empty habitat with correct width and height', () => {
+  const habitat = Habitat.empty(12, 16);
+  expect(habitat.w).toEqual(12);
+  expect(habitat.h).toEqual(16);
+  expect(habitat.cells).toHaveLength(16);
+
+  for (let j = 0; j < habitat.h; j += 1) {
+    expect(habitat.cells[j]).toHaveLength(12);
+  }
+});
+
+test('cannot create empty habitat with zero columns', () => {
+  expect(() => {
+    Habitat.empty(0, 15);
+  }).toThrow(Habitat.ZeroSizeHabitat);
+
+  expect(() => {
+    Habitat.empty(11, 0);
+  }).toThrow(Habitat.ZeroSizeHabitat);
+
+  expect(() => {
+    Habitat.empty(0, 0);
+  }).toThrow(Habitat.ZeroSizeHabitat);
+});
+
+test('fromCells accepts valid array', () => {
+  const originalArray = [[1, 0, 1], [0, 1, 0], [1, 1, 1]];
+  const habitat = Habitat.fromCells(originalArray);
+
+  expect(habitat).toBeDefined();
+  expect(originalArray).toEqual(habitat.cells);
+});
+
+test('fromCells does not accept empty arrays', () => {
+  expect(() => {
+    Habitat.fromCells([]);
+  }).toThrow(Habitat.EmptyArrayError);
+});
+
+test('fromCells does not accept malformed arrays', () => {
+  expect(() => {
+    Habitat.fromCells([[0, 1, 0], [0, 1], [1]]);
+  }).toThrow(Habitat.MalformedArrayError);
+});
+
+test('fromCells does not accept arrays with values other than 0 or 1', () => {
+  expect(() => {
+    Habitat.fromCells([[123, 0, -1], [65535, 1, 0], [1, 1, 1]]);
+  }).toThrow(Habitat.MalformedArrayError);
+});
+
+test('habitat cells can be accessed and access has a toroidal property', () => {
+  const originalArray = [
+    [1, 0, 1],
+    [0, 1, 0],
+    [0, 1, 1],
+  ];
+  const habitat = Habitat.fromCells(originalArray);
+  expect(habitat).toBeDefined();
+
+  for (let j = 0; j < 3; j += 1) {
+    for (let i = 0; i < 3; i += 1) {
+      expect(habitat.aliveAt(i, j)).toEqual(originalArray[j][i] > 0);
+    }
+  }
+
+  expect(habitat.aliveAt(-1, -1)).toEqual(true);
+  expect(habitat.aliveAt(0, -1)).toEqual(false);
+  expect(habitat.aliveAt(1, -1)).toEqual(true);
+  expect(habitat.aliveAt(2, -1)).toEqual(true);
+  expect(habitat.aliveAt(3, -1)).toEqual(false);
+
+  expect(habitat.aliveAt(3, 0)).toEqual(true);
+  expect(habitat.aliveAt(3, 1)).toEqual(false);
+  expect(habitat.aliveAt(3, 2)).toEqual(false);
+  expect(habitat.aliveAt(3, 3)).toEqual(true);
+
+  expect(habitat.aliveAt(-1, 3)).toEqual(true);
+  expect(habitat.aliveAt(0, 3)).toEqual(true);
+  expect(habitat.aliveAt(1, 3)).toEqual(false);
+  expect(habitat.aliveAt(2, 3)).toEqual(true);
+
+  expect(habitat.aliveAt(-1, 0)).toEqual(true);
+  expect(habitat.aliveAt(-1, 1)).toEqual(false);
+  expect(habitat.aliveAt(-1, 2)).toEqual(true);
+  expect(habitat.aliveAt(-1, 3)).toEqual(true);
+});
+
+test('neighbor count is correct', () => {
+  const originalArray = [
+    [1, 0, 1],
+    [0, 1, 0],
+    [0, 1, 1],
+  ];
+  const habitat = Habitat.fromCells(originalArray);
+  expect(habitat).toBeDefined();
+
+  expect(habitat.neighbourCount(0, 0)).toEqual(4);
+  expect(habitat.neighbourCount(1, 0)).toEqual(5);
+  expect(habitat.neighbourCount(2, 0)).toEqual(4);
+
+  expect(habitat.neighbourCount(0, 1)).toEqual(5);
+  expect(habitat.neighbourCount(1, 1)).toEqual(4);
+  expect(habitat.neighbourCount(2, 1)).toEqual(5);
+
+  expect(habitat.neighbourCount(0, 2)).toEqual(5);
+  expect(habitat.neighbourCount(1, 2)).toEqual(4);
+  expect(habitat.neighbourCount(2, 2)).toEqual(4);
+});
+
+test('map works correctly', () => {
+  const originalArray = [
+    [1, 0, 1],
+    [0, 1, 0],
+    [0, 1, 1],
+  ];
+  const habitat = Habitat.fromCells(originalArray);
+  expect(habitat.cells).toEqual(originalArray);
+
+  const newHabitat1 = habitat.map(() => true);
+  expect(newHabitat1.cells).toEqual([[1, 1, 1], [1, 1, 1], [1, 1, 1]]);
+
+  const newHabitat2 = habitat.map(() => false);
+  expect(newHabitat2.cells).toEqual([[0, 0, 0], [0, 0, 0], [0, 0, 0]]);
+
+  let flop = true;
+  const newHabitat3 = habitat.map(() => {
+    flop = !flop;
+    return flop;
+  });
+  expect(newHabitat3.cells).toEqual([[0, 1, 0], [1, 0, 1], [0, 1, 0]]);
+});
---
+++ gameoflife/habitat.js
@@ -0,0 +1,159 @@
+/**
+ * Thrown when an empty array is provided as a paramter for creation factory functions.
+ */
+class EmptyArrayError extends Error {
+  constructor() {
+    super('fromCells does not accept empty arrays');
+  }
+}
+
+/**
+ * Thrown when the array provided is malformed in some regard (bad dimensions, values).
+ */
+class MalformedArrayError extends Error {
+  constructor() {
+    super('array must have the proper dimension and contain only boolean values');
+  }
+}
+
+/**
+ * Thrown when the callee attempts to create an empty habitat.
+ */
+class ZeroSizeHabitat extends Error {
+  constructor() {
+    super('cannot create zero length habitat');
+  }
+}
+
+function emptyArrayWithDimensions(w, h) {
+  const arr = new Array(h);
+  for (let j = 0; j < h; j += 1) {
+    arr[j] = new Array(w);
+  }
+  return arr;
+}
+
+class Habitat {
+  /**
+   * Creates a new habitat.
+   * @param w number of cells horizontally
+   * @param h number of cells vertically
+   * @param cells initial cell configuration, if not specified create an empty habitat
+   */
+  constructor(w, h, cells) {
+    this.w = w;
+    this.h = h;
+
+    if (cells) {
+      this.cells = cells;
+    } else {
+      this.cells = emptyArrayWithDimensions(w, h);
+    }
+  }
+
+  /**
+   * Check whether there is an alive cell at the specified coordinates.
+   * @param x horizontal position
+   * @param y vertical position
+   * @returns {boolean} if cell is alive, false otherwise
+   */
+  aliveAt(x, y) {
+    let fx = (x % this.w);
+    let fy = (y % this.h);
+    if (fx < 0) {
+      fx = this.w + fx;
+    }
+    if (fx >= this.w) {
+      fx -= this.w;
+    }
+    if (fy < 0) {
+      fy = this.h + fy;
+    }
+    if (fy >= this.h) {
+      fy -= this.h;
+    }
+
+    return this.cells[fy][fx] > 0;
+  }
+
+  /**
+   * Counts the number of neighbours at the specified position.
+   * @param x horizontal position
+   * @param y vertical position
+   * @returns {int} neighbour count
+   */
+  neighbourCount(x, y) {
+    return this.aliveAt(x - 1, y - 1) + this.aliveAt(x, y - 1) + this.aliveAt(x + 1, y - 1) +
+      this.aliveAt(x - 1, y) + this.aliveAt(x + 1, y) +
+      this.aliveAt(x - 1, y + 1) + this.aliveAt(x, y + 1) + this.aliveAt(x + 1, y + 1);
+  }
+
+  /**
+   * Execute mapping function on cells and create a new habitat from the results.
+   * @param fn mapping function gets neighbor count as first parameter and
+   * returns whether the cell is alive or dead
+   * @returns {Habitat} new habitat containing the resulting cells
+   */
+  map(fn) {
+    const result = emptyArrayWithDimensions(this.w, this.h);
+    for (let j = 0; j < this.h; j += 1) {
+      for (let i = 0; i < this.w; i += 1) {
+        result[j][i] = fn(this.neighbourCount(i, j)) ? 1 : 0;
+      }
+    }
+    return Habitat.fromCells(result);
+  }
+
+  /**
+   * Creates a new habitat containing the specified cells.
+   * @param cells cells encoded as an array of rows
+   * @returns {Habitat} habitat instance containing the cells
+   */
+  static fromCells(cells) {
+    if (cells.length === 0) {
+      throw new EmptyArrayError();
+    }
+
+    const w = cells[0].length;
+    const h = cells.length;
+
+    for (let j = 1; j < h; j += 1) {
+      if (cells[j].length !== w) {
+        throw new MalformedArrayError();
+      }
+      for (let i = 0; i < w; i += 1) {
+        if (![0, 1].includes(cells[j][i])) {
+          throw new MalformedArrayError();
+        }
+      }
+    }
+
+    return new Habitat(w, h, cells);
+  }
+
+  /**
+   * Create an empty habitat with the specified width and height.
+   * @param w habitat width
+   * @param h habitat height
+   * @returns {Habitat} empty habitat instance
+   */
+  static empty(w, h) {
+    if (w === 0) {
+      throw new ZeroSizeHabitat();
+    }
+
+    if (h === 0) {
+      throw new ZeroSizeHabitat();
+    }
+
+    return new Habitat(w, h);
+  }
+}
+
+module.exports = {
+  fromCells: Habitat.fromCells,
+  empty: Habitat.empty,
+  EmptyArrayError,
+  MalformedArrayError,
+  ZeroSizeHabitat,
+};
