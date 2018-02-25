ec8be8ab6c5645a01b4564aa24c788ec79b0cf0c
Gabriel Miklós <socketbind@gmail.com>
2018-02-25 15:52:28 +0100

    - Some docs for utils. - Add accidentally left out simple_lines.txt

--- gameoflife/utils.js
+++ gameoflife/utils.js
@@ -1,11 +1,20 @@
 const fs = require('fs');
 
+/**
+ * Thrown when the user is attempting to create an invalid array (zero or negative dimensions).
+ */
 class InvalidArraySizeError extends Error {
   constructor() {
-    super("creating zero dimension array is not allowed");
+    super('creating zero or negative dimension array is not allowed');
   }
 }
 
+/**
+ * Reads the specified file.
+ * @param path relative or absolute path of the file
+ * @returns {Promise<Array<string>>} promise resolving to an string array of lines or
+ * rejects with an error
+ */
 function readLines(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
@@ -18,6 +27,13 @@ function readLines(path) {
   });
 }
 
+/**
+ * Creates an empty array with the specified with an height. Values are undefined
+ * by default (as is with Array(n)). Rows can be accessed by the first index.
+ * @param w width of the array
+ * @param h height of the array
+ * @returns {any[]} array instance with the specified dimensions
+ */
 function emptyArrayWithSize(w, h) {
   if (w === 0 || h === 0) {
     throw new InvalidArraySizeError();
